d4b506358b94f17a2a14c49e92835c70984339e7
Huanghc <1803655330@qq.com>
2026-05-28 22:06:09 +0800

    更改地图位置和data信息

--- data/data.js
+++ data/data.js
@@ -4,12 +4,12 @@ module.exports = {
   weatherKey: 'b1dd16e6c51f4d98906fb6fead2af8b2',
 
   // 小程序名称
-  miniprogram_name: '桂院校园导航',
+  miniprogram_name: '嘉应校园导航',
 
   // 相关信息
   information: {
-    type: '计算机设计大赛作品 | 毕业设计',
-    author: '理工学院 2019级 软件工程 吴警',
-    teacher: '刘亮龙、杨嫘 | 韦林',
+    type: '软件工程课题设计',
+    author: '黄浩城(231110271)、莫顺彬(231110293)、罗子钦(231110303)、黄湛文(231110306)、罗春妮(231110310)、朱祖勋(231110294)',
+    teacher: '魏彩珍',
   },
 };
